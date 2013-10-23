db9d49e57674f54a4d1c86605f6114e706c91e9a
Tom Meagher <hello+git@tommeagher.com>
2013-10-23 10:19:00 -0400

    added support for WP pages

--- unscrew.py
+++ unscrew.py
@@ -4,17 +4,25 @@ from local_settings import SECRET_KEY, SITE_URL
 app = Flask(__name__)
 #app.config.from_object(__name__)
 
+#redirect the main page
 @app.route('/')
 def index():
     return(redirect(SITE_URL))
 
-@app.route('/<int:year>/<int:month>/<path:slug>')
+#redirect the Wordpress posts to new site
+@app.route('/<int:year>/<int:month>/<path:slug>/')
 def posts(year, month, slug):
     return(redirect(SITE_URL+'/{0}/{1}/{2}.html'.format(year, month, slug)))
 
+#redirect the Wordpress pages to the new site
+@app.route('/<path:slug>/')
+def pages(slug):
+    return(redirect(SITE_URL+'/{0}.html'.format(slug)))
+
+#anything that's not caught gets thrown to the 404.
 @app.errorhandler(404)
 def page_not_found(error):
-    return "I moved the site, Try this"
+    return(redirect(SITE_URL+"/404/"))
 
 if __name__ == '__main__':
     #app.debug = True
